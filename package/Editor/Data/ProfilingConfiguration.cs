c4ba4d71614e5b2b54ce6e16f307a8b94e6c7c06
Marcel Wiessler <marcel@gaisterhand.de>
2021-03-16 11:28:37 +0100

    minor

--- package/Editor/Data/ProfilingConfiguration.cs
+++ package/Editor/Data/ProfilingConfiguration.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Needle.SelectiveProfiling
 {
 	[Serializable]
 	internal class ProfilingConfiguration
 	{
+		[SerializeField]
+		private string Id;
 		public string Name;
-		public List<MethodInformation> Methods;
+		public List<MethodInformation> Methods = new List<MethodInformation>();
+		public List<MethodInformation> Muted = new List<MethodInformation>();
+
+		public ProfilingConfiguration(string name)
+		{
+			this.Name = name;
+			this.Id = Guid.NewGuid().ToString();
+		}
 	}
 }
