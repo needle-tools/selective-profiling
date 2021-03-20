13942e2cf65d9e5b5260e0bdee0c9732243f905b
Marcel Wiessler <marcel@gaisterhand.de>
2021-03-20 13:18:50 +0100

    quick test using HarmonyX

---
+++ projects/HarmonyX/Assets/MyScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using HarmonyLib.Public.Patching;
+using MonoMod.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyScript : MonoBehaviour
+{
+    public Text TextField;
+    
+    private void Start()
+    {
+        
+        Harmony.CreateAndPatchAll(typeof(MyScript));
+        // var h = new HarmonyLib.Harmony("test");
+        // PatchManager.ResolvePatcher += 
+        // h.PatchAll();
+        il2cppchain
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextField.text = GetText();
+    }
+
+    public string GetText()
+    {
+        return "didnt work";
+    }
+
+    
+    [HarmonyPostfix]
+    [HarmonyPatch( typeof(MyScript), "GetText")]
+    private static void Postfix(ref string __result)
+    {
+        __result = "patch worked";
+    }
+
+    // private class Patcher : MethodPatcher
+    // {
+    //     public Patcher(MethodBase original) : base(original)
+    //     {
+    //         
+    //     }
+    //
+    //     public override DynamicMethodDefinition PrepareOriginal()
+    //     {
+    //         
+    //     }
+    //
+    //     public override MethodBase DetourTo(MethodBase replacement)
+    //     {
+    //     }
+    //
+    //     public override DynamicMethodDefinition CopyOriginal()
+    //     {
+    //     }
+    // }
+}
