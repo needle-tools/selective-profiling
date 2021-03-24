fa11488391b90615d1f99849b709858911070e93
Marcel Wiessler <marcel@gaisterhand.de>
2021-03-24 15:16:27 +0100

    always allow to be enabled

--- package/Editor/SelectiveProfiler.cs
+++ package/Editor/SelectiveProfiler.cs
@@ -88,7 +88,7 @@ namespace Needle.SelectiveProfiling
 		/// <summary>
 		/// check editor state (this does not settings enabled state)
 		/// </summary>
-		internal static bool AllowToBeEnabled => !ProfilerDriver.deepProfiling || DevelopmentMode;
+		internal static bool AllowToBeEnabled => true;// !ProfilerDriver.deepProfiling || DevelopmentMode;
 
 		private static async Task InternalEnableProfilingAsync(
 			MethodInfo method,
