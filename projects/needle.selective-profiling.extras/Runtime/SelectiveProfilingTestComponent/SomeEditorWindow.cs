18f43b13719f15317b7c488727ac9deb1b39e519
Marcel Wiessler <marcel@gaisterhand.de>
2021-03-25 21:21:54 +0100

    start some IL debugging + opcode cheatsheet

---
+++ projects/needle.selective-profiling.extras/Runtime/SelectiveProfilingTestComponent/SomeEditorWindow.cs
@@ -0,0 +1,67 @@
+﻿#if UNITY_EDITOR
+
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace SelectiveProfilingTestComponent
+{
+	public class SomeEditorWindow : EditorWindow
+	{
+		private void OnGUI()
+		{
+			SomethingWithArgs(5);
+			
+			// bool test = true;
+			bool selected = false;
+			string label = "test";
+			// bool myval = test;
+
+			if (IsRenaming())
+			{
+				selected = true;
+				label = "";
+			}
+
+			if (Event.current.type == EventType.Repaint && IsRenaming("calling a method"))
+			{
+				Debug.Log("is repaint " + label);
+			}
+			// else Debug.Log(Event.current);
+
+			string marker = "test 2 with false";
+			if (!IsRenaming())
+			{
+				selected = true;
+				label = "";
+				return;
+			}
+			
+			
+			Debug.Log("is never called " + label + ", " + selected);
+
+			for (int i = 0; i < 5; i++)
+			{
+				Debug.Log(i);
+			}
+		}
+
+		private bool SomethingWithArgs(int i)
+		{
+			i += 2;
+			return true;
+		}
+
+		[MenuItem("Test/EditorWindowWithRepaint")]
+		private static void Test()
+		{
+			CreateInstance<SomeEditorWindow>().Show();
+		}
+
+		private bool IsRenaming() => false;
+		private bool IsRenaming(string test) => false;
+	}
+}
+
+#endif
