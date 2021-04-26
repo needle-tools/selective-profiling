367b90fb0cb536c9a46dff1ebedda07cf6c33480
Marcel Wiessler <marcel@gaisterhand.de>
2021-04-26 16:36:53 +0200

    constrained test code

--- projects/needle.selective-profiling.extras/Runtime/SelectiveProfilingTestComponent/TestVirt.cs
+++ projects/needle.selective-profiling.extras/Runtime/SelectiveProfilingTestComponent/TestVirt.cs
@@ -1,33 +1,52 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using UnityEditor;
+﻿using UnityEngine;
 
 namespace SelectiveProfilingTestComponent
 {
-	public class TestConstrainedCall
+	public class TestVirt
 	{
-		private static void DoCall()
+		private static void DoCall(MyObj<IMyInterface> obj)
 		{
-			string str = null;
-			foreach (var num in Enumerate())
+			Debug.Log(new MyStruct().ToString());
+			// var res = obj.Run();
+			// Debug.Log(res);
+			// string str = null;
+			// foreach (var num in Enumerate())
+			// {
+			// 	// str = num.ToString();
+			// 	// var calc = 1 + 2;
+			// 	// str = TakeAny(str);
+			// }
+		}
+
+		// public static T TakeAny<T>(T str)
+		// {
+		// 	return str;
+		// }
+		//
+		// public static IEnumerable<MyObj<string>> Enumerate()
+		// {
+		// 	return new[] {new MyObj<string>()};
+		// }
+
+		public abstract class MyObj<T>
+		{
+			public T Run()
 			{
-				str = num.ToString();
-				var calc = 1 + 2;
-				str = TakeAny(str);
+				return default;
 			}
 		}
-
-		public static T TakeAny<T>(T str)
+		
+		public interface IMyInterface
 		{
-			return str;
+			
 		}
 
-		public static IEnumerable<MyObj> Enumerate()
+		public struct MyStruct
 		{
-			return new[] {new MyObj()};
+			
 		}
 
-		public class MyObj
+		public class MyObjImpl : MyObj<string>
 		{
 			
 		}
